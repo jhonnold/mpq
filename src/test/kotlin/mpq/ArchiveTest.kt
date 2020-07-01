3bf34828fbf7ca0f6affe0a4e41c63eb33c8e248
Jay Honnold <jayhonnold@gmail.com>
2020-06-30 20:53:19 -0500

    Refactor data parsing

--- src/test/kotlin/mpq/ArchiveTest.kt
+++ src/test/kotlin/mpq/ArchiveTest.kt
@@ -1,29 +1,27 @@
 package mpq
 
 import org.junit.Test
+import java.nio.ByteBuffer
+import java.nio.file.Paths
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 class ArchiveTest {
-    @Test fun init() {
-        val pathname = this.javaClass.getResource("/archive")!!.file;
-        val archive = Archive(pathname);
+    @Test
+    fun init() {
+        val projectDirPath = Paths.get("").toAbsolutePath()
+        val resourcePath = Paths.get(projectDirPath.toString(), "/src/test/resources/archive")
 
-        assertNotNull(archive.header.userData)
-        assertEquals(512, archive.header.userData!!.getUserDataSize())
-        assertEquals(1024, archive.header.userData!!.getHeaderOffset())
-        assertEquals(115, archive.header.userData!!.getUserDataHeaderSize())
-
-        println(archive.header.userData!!.getContent())
-
-        assertEquals(208, archive.header.getHeaderSize())
-        assertEquals(68730, archive.header.getArchiveSize())
-        assertEquals(3, archive.header.getFormatVersion())
-        assertEquals(5, archive.header.getSectorSizeShift())
-        assertEquals(67946, archive.header.getHashTableOffset())
-        assertEquals(68458, archive.header.getBlockTableOffset())
-        assertEquals(32, archive.header.getHashTableEntries())
-        assertEquals(17, archive.header.getBlockTableEntries())
-        assertEquals(1024, archive.header.offset)
+        Archive(resourcePath).use {
+            val userData = it.userData
+            assertNotNull(userData)
+            assertEquals(
+                    ByteBuffer.wrap(byteArrayOf('M'.toByte(), 'P'.toByte(), 'Q'.toByte(), 0x1B)).position(4),
+                    userData.magic)
+            assertEquals(512, userData.userDataSize)
+            assertEquals(1024, userData.headerOffset)
+            assertEquals(115, userData.userDataHeaderSize)
+        }
     }
 }
